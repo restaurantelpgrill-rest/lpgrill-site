d5747e0a501b49f1706b3c75820030cd25f081d6
restaurantelpgrill-rest <restaurantelpgrill@gmail.com>
2026-02-20 15:43:13 -0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,26 +1,21 @@
 window.DATA = {
-
   marmitas: [
-    { id:"m1", title:"Marmita Tradicional", desc:"Arroz, feijão e carne", price:18.90, img:"img/marmita_tradicional.jpg" },
-    { id:"m2", title:"Marmita Frango", desc:"Frango grelhado", price:21.90, img:"img/marmita_frango.jpg" },
-    { id:"m3", title:"Marmita Carne", desc:"Carne acebolada", price:23.90, img:"img/marmita_carne.jpg" },
-    { id:"m4", title:"Marmita Especial", desc:"Completa", price:24.90, img:"img/mockup.png" }
+    { id:"m1", title:"Marmita Tradicional", desc:"Arroz, feijão, carne e salada.", tag:"Bem servida", price:18.90, img:"img/marmita_tradicional.jpg" },
+    { id:"m2", title:"Marmita Frango Grelhado", desc:"Frango grelhado + acompanhamentos.", tag:"Leve", price:21.90, img:"img/marmita_frango.jpg" },
+    { id:"m3", title:"Marmita Carne", desc:"Carne acebolada + acompanhamentos.", tag:"Top", price:23.90, img:"img/marmita_carne.jpg" },
+    { id:"m4", title:"Marmita do Chef", desc:"Especial do dia • bem montada.", tag:"Especial", price:24.90, img:"img/mockup.png" }
   ],
-
   porcoes: [
-    { id:"p1", title:"Batata Frita", desc:"Crocante", price:17.90, img:"img/mockup.png" },
-    { id:"p2", title:"Frango Frito", desc:"Suculento", price:24.90, img:"img/frango_grelhado.jpg" }
+    { id:"p1", title:"Porção de Batata Frita", desc:"Crocante • perfeita pra compartilhar.", tag:"Clássica", price:17.90, img:"img/mockup.png" },
+    { id:"p2", title:"Porção de Frango", desc:"Frango dourado • suculento.", tag:"Caprichada", price:24.90, img:"img/frango_grelhado.jpg" }
   ],
-
   bebidas: [
-    { id:"b1", title:"Coca-Cola Lata", desc:"350ml", price:6, img:"img/coca_lata.jpg" },
-    { id:"b2", title:"Guaraná Lata", desc:"350ml", price:6, img:"img/guarana_lata.jpg" },
-    { id:"b3", title:"Água", desc:"500ml", price:4, img:"img/agua_500.jpg" },
-    { id:"b4", title:"Água com gás", desc:"500ml", price:5, img:"img/agua_500.jpg" }
+    { id:"b1", title:"Coca-Cola Lata", desc:"350ml gelada.", tag:"350ml", price:6.00, img:"img/coca_lata.jpg" },
+    { id:"b2", title:"Guaraná Lata", desc:"350ml gelada.", tag:"350ml", price:6.00, img:"img/guarana_lata.jpg" },
+    { id:"b3", title:"Água 500ml", desc:"Sem gás.", tag:"500ml", price:4.00, img:"img/agua_500.jpg" },
+    { id:"b4", title:"Água com gás", desc:"500ml gelada.", tag:"500ml", price:5.00, img:"img/agua_500.jpg" }
   ],
-
   sobremesas: [
-    { id:"s1", title:"Sobremesa da casa", desc:"Doce especial", price:9.90, img:"img/mockup.png" }
+    { id:"s1", title:"Sobremesa da Casa", desc:"Finalize com chave de ouro.", tag:"Doce", price:9.90, img:"img/mockup.png" }
   ]
-
 };
